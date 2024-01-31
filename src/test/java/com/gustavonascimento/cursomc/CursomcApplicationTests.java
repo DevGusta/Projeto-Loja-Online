28c91e7251a018169f81c7f7cbf284fc57face30
Gustavo Santos <gunasc2014@gmail.com>
2024-01-31 11:54:42 -0300

    Projeto criado

---
+++ src/test/java/com/gustavonascimento/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavonascimento.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
